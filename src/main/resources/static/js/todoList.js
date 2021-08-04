04daa0b9d289752698efba56f5944b85608ff3f2
Kihyeon Kim <kiheyunkim@gmail.com>
2021-08-04 22:41:43 +0900

    js 통합 및 html하단으로 스크립트 태그 이동

--- src/main/resources/static/js/todoList.js
+++ src/main/resources/static/js/todoList.js
@@ -1,8 +1,34 @@
 $(document).ready(() => {
-    $('.plus').click(() => {
+    document.getElementById('now_date').valueAsDate = new Date();
+
+    $('.plus').click(function () {
         let today = new Date();
         $('#now_date').attr('min', today.toISOString().split('T')[0]);
+
+        $("section").addClass("popupDown");
+        $("section").removeClass("popupUp");
+        $(".plus").addClass("hide");
+        $(".cancel").addClass("show");
     });
+
+    $(".cancel").click(function () {
+            $("section").toggleClass("popupDown");
+            $("section").toggleClass("popupUp");
+            $(".plus").removeClass("hide");
+            $(".cancel").removeClass("show");
+        }
+    );
+
+    // 특정 날을 달력에서 클릭하면 다른 날 클릭 비활성화
+    $(".calendar-body tr td").click(function () {
+            if ($(this).text().length === 0) {
+                return;
+            }
+            $(this).addClass("dayClick");
+            $(this).siblings().removeClass("dayClick");
+            $(this).parent().siblings().children().removeClass("dayClick");
+        }
+    );
 
     let checkDate = (target) => {     //날짜가 올바른지 최종 체크
         let today = new Date();
